99b31f0cfd090dd7176ffe965c0104f763fde81a
TJ Ellis <thomasjayellis@gmail.com>
2014-08-22 14:30:19 -0400

    - added alarm sound on stir and finish

--- src/org/ellisto/thecoffeeapp/TimerActivity.java
+++ src/org/ellisto/thecoffeeapp/TimerActivity.java
@@ -1,16 +1,27 @@
 package org.ellisto.thecoffeeapp;
 
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TimerActivity extends ActionBarActivity {
 	TextView timerTextView, nextStepTextView;
+	Button stopAlarmButton;
 	int targetMin = 4;
 	int targetSec = 0;
 	long targetMs = 0;
-	long stirTimeMs = 60000;
+	boolean stirAlerted = false;
+	long stirTimeMs = 1000;// 60000;
+
+	MediaPlayer alarmMp = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,7 @@ public class TimerActivity extends ActionBarActivity {
 
 		timerTextView = (TextView) findViewById(R.id.timerTextView);
 		nextStepTextView = (TextView) findViewById(R.id.nextStepTextView);
+		stopAlarmButton = (Button) findViewById(R.id.stopAlarmButton);
 
 		targetMs = targetMin * 60000 + targetSec * 1000;
 
@@ -32,7 +44,8 @@ public class TimerActivity extends ActionBarActivity {
 				timerTextView.setText(String
 						.format("%d:%02d", minutes, seconds));
 
-				if (remainingMs <= targetMs - stirTimeMs) {
+				if (!stirAlerted && remainingMs <= targetMs - stirTimeMs) {
+					stirAlerted = true;
 					alertStir();
 				}
 			}
@@ -41,7 +54,8 @@ public class TimerActivity extends ActionBarActivity {
 				timerTextView.setText("0:00");
 				// TODO: pull this from a resource
 				nextStepTextView.setText("Press and enjoy!");
-				// TODO: play a sound, vibrate
+				playAlarmSound();
+				// TODO: vibrate
 			}
 		}.start();
 	}
@@ -49,8 +63,33 @@ public class TimerActivity extends ActionBarActivity {
 	protected void alertStir() {
 		// TODO: pull this from a resource
 		nextStepTextView.setText("Time to stir and add the rest of the water!");
+		playAlarmSound();
+		// TODO: vibrate
+	}
 
-		// TODO: play a sound, vibrate
+	protected void playAlarmSound() {
+		Uri notification = RingtoneManager
+				.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		alarmMp = MediaPlayer.create(getApplicationContext(), notification);
+		try {
+			alarmMp.prepare();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		alarmMp.setLooping(true);
+		alarmMp.start();
+		stopAlarmButton.setVisibility(View.VISIBLE);
+	}
 
+	public void stopAlarm(View view) {
+		if (alarmMp != null) {
+			alarmMp.stop();
+			alarmMp = null;
+		}
+		stopAlarmButton.setVisibility(View.INVISIBLE);
 	}
 }
