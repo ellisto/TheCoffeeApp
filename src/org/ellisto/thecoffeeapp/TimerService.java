3c12a203b77a82150f74a264a382a28207949cf1
TJ Ellis <thomasjayellis@gmail.com>
2014-08-23 10:18:23 -0400

    - made timer text large and readable.  - made stir timer set to realistic 1 minute  - cleaned up some commented code

--- src/org/ellisto/thecoffeeapp/TimerService.java
+++ src/org/ellisto/thecoffeeapp/TimerService.java
@@ -10,33 +10,12 @@ public class TimerService extends Service {
 	int targetSec = 0;
 	long targetMs = 0;
 	boolean stirAlerted = false;
-	long stirTimeMs = 3000;// 60000;
+	long stirTimeMs = 60000;
 
 	public static final String NOTIFICATION = "org.ellisto.thecoffeeapp.receiver";
 	public static final String MILLIS_REMAINING = "org.ellisto.thecoffeeapp.MINUTES_REMAINING";
 	public static final String STIR_ALERT = "org.ellisto.thecoffeeapp.STIR_ALERT";
 
-	// @Override
-	// protected void onHandleIntent(Intent workIntent) {
-	// targetMs = targetMin * 60000 + targetSec * 1000;
-	//
-	// new CountDownTimer(targetMs, 1000) {
-	//
-	// public void onTick(long remainingMs) {
-	//
-	// broadcastTimerUpdate(remainingMs);
-	//
-	// if (!stirAlerted && remainingMs <= targetMs - stirTimeMs) {
-	// stirAlerted = true;
-	// broadcastStirAlert();
-	// }
-	// }
-	//
-	// public void onFinish() {
-	// broadcastTimerUpdate(0);
-	// }
-	// }.start();
-	// }
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		targetMs = targetMin * 60000 + targetSec * 1000;
